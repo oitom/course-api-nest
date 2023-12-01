db5f34596d19317a5c21782563975723934109dd
Wellington Costa <wcosta.ale@gmail.com>
2023-12-01 10:13:07 -0300

    Configurando entidades com o typeORM

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceOptions } from 'typeorm';
+import { Course } from '../courses/entities/courses.entity'
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -9,8 +10,8 @@ export const dataSourceOptions: DataSourceOptions = {
   username: 'postgres',
   password: 'docker',
   database: 'devtraing',
-  entities: [],
-  synchronize: true // sincroniza as propriedades do entity como coluna no bd
+  entities: [Course],
+  synchronize: true, // sincroniza as propriedades do entity como coluna no bd
 }
 
 @Module({
